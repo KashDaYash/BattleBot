aacf1d87f09c6250a57110fd4722c6b5cf1ea251
KashDaYash <85399597+KashDaYash@users.noreply.github.com>
2025-05-18 23:33:51 +0530

    Update __main__.py

--- yash/__main__.py
+++ yash/__main__.py
@@ -1,39 +1,41 @@
 import asyncio
-from yash import app, user_db
 import importlib
-from yash.modules import ALL_MODULES
-from pyrogram import idle
-import config
 from random import choice
+
+from pyrogram import idle
+from yash import app
+from yash.core import database
 from yash.data.characters import CHARACTERS
+from yash.modules import ALL_MODULES
+import config
 
 HELPABLE = {}
 
 async def init():
+    # Init MongoDB collection
+    database.init_db("users")
+
+    # Load modules with help text
     for module_name in ALL_MODULES:
         imported_module = importlib.import_module(module_name)
         if hasattr(imported_module, "__MODULE__") and imported_module.__MODULE__:
             if hasattr(imported_module, "__HELP__") and imported_module.__HELP__:
                 HELPABLE[imported_module.__MODULE__.lower()] = imported_module
-    print("sᴜᴄᴄᴇssғᴜʟʟʏ ɪᴍᴘᴏʀᴛᴇᴅ ᴍᴏᴅᴜʟᴇs...") 
-    user_db.set_table("users")
-    user_db.create_table("users", {
-        "user_id": "INTEGER",
-        "character": "TEXT",
-        "level": "INTEGER DEFAULT 1",
-        "xp": "INTEGER DEFAULT 0"
-    }, primary_key="user_id")
-    existing = user_db.find_by(user_id=config.OWNER_ID)
-    
-    if not existing:
+    print("sᴜᴄᴄᴇssғᴜʟʟʏ ɪᴍᴘᴏʀᴛᴇᴅ ᴍᴏᴅᴜʟᴇs...")
+
+    # Check if OWNER exists in DB
+    owner_exists = await database.exists({"user_id": config.OWNER_ID})
+
+    if not owner_exists:
         random_character = choice(list(CHARACTERS.keys()))
-        user_db.insert({
+        await database.insert({
             "user_id": config.OWNER_ID,
             "character": random_character,
             "level": 1,
             "xp": 0
         })
-    
+
+    # Start bot
     await app.start()
     await app.send_message(config.LOGGER_ID, "Alive")
     print("Bot Started. Waiting for events...")
@@ -41,4 +43,4 @@ async def init():
     await app.stop()
 
 if __name__ == "__main__":
-    asyncio.get_event_loop_policy().get_event_loop().run_until_complete(init())
+    asyncio.run(init())
