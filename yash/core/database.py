64a797009f9602c7cc7db871c3e18ae5b8c452b6
KashDaYash <85399597+KashDaYash@users.noreply.github.com>
2025-06-16 14:29:37 +0530

    Update database.py

--- yash/core/database.py
+++ yash/core/database.py
@@ -1,35 +1,8 @@
-import motor.motor_asyncio
+from motor.motor_asyncio import AsyncIOMotorClient
 import config 
 
-client = motor.motor_asyncio.AsyncIOMotorClient(config.MONGO_DB_URI)
-db = client["users"]
+mongo_url = "mongodb://localhost:27017"  # ya aapka Mongo Atlas URI
+client = AsyncIOMotorClient(config.MONGO_DB_URI)
 
-collection = db["usercol"]  # define globally
-
-def init_db(collection_name):
-    global collection
-    collection = db[collection_name]
-
-async def insert(data: dict):
-    if collection is None:
-        raise RuntimeError("Database not initialized. Call init_db() first.")
-    result = await collection.insert_one(data)
-    return result.inserted_id
-
-async def update(filters: dict, updates: dict, upsert=False):
-    if not collection:
-        raise RuntimeError("Database not initialized. Call init_db() first.")
-    result = await collection.update_many(filters, {"$set": updates}, upsert=upsert)
-    return result.modified_count
-
-async def delete(filters: dict):
-    if not collection:
-        raise RuntimeError("Database not initialized. Call init_db() first.")
-    result = await collection.delete_many(filters)
-    return result.deleted_count
-
-async def exists(query: dict) -> bool:
-    if collection is None:
-        raise ValueError("Collection is not set.")
-    result = await collection.find_one(query)
-    return result is not None
+db = client["yashigame"]
+users = db["users"]
