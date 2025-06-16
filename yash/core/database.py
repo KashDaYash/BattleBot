89adfaec35ed1a62ea3f3622ec3526da3510cbd1
KashDaYash <85399597+KashDaYash@users.noreply.github.com>
2025-06-16 14:29:55 +0530

    Update database.py

--- yash/core/database.py
+++ yash/core/database.py
@@ -1,7 +1,6 @@
 from motor.motor_asyncio import AsyncIOMotorClient
 import config 
 
-mongo_url = "mongodb://localhost:27017"  # ya aapka Mongo Atlas URI
 client = AsyncIOMotorClient(config.MONGO_DB_URI)
 
 db = client["yashigame"]
